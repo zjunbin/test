4708e38cc5ae2cb075a40fcc3a7e9551610d7ab2
Your Namezjunbin <648260307@qq.com>
2019-04-02 16:07:01 +0800

    cs

---
+++ api/__init__.py
@@ -0,0 +1,5 @@
+#  coding utf-8
+# @time      :2019/4/215:27
+# @Author    :zjunbin
+# @Email     :648060307@qq.com
+# @File      :__init__.py.py
---
+++ api/testing.py
@@ -0,0 +1,9 @@
+#  coding utf-8
+# @time      :2019/4/215:27
+# @Author    :zjunbin
+# @Email     :648060307@qq.com
+# @File      :testing.py
+print('222222')
+print('222222')
+print('222222')
+print('222222')
